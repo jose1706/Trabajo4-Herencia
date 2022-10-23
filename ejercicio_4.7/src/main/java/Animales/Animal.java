9e47c2235b7454dbe2704b5743fdaa40a4ecff42
Jeronimo <111191077+Jeronimo02@users.noreply.github.com>
2022-10-22 21:38:25 -0500

    Add files via upload

---
+++ ejercicio_4.7/src/main/java/Animales/Animal.java
@@ -0,0 +1,12 @@
+package Animales;
+
+public abstract class Animal {
+    protected String sonido;
+    protected String alimentos;
+    protected String hábitat;
+    protected String nombreCientífico;
+    public abstract String getNombreCientífico();
+    public abstract String getSonido();
+    public abstract String getAlimentos();
+    public abstract String getHábitat();
+}
---
+++ ejercicio_4.7/src/main/java/Animales/Cánido.java
@@ -0,0 +1,7 @@
+package Animales;
+/**
+* Esta clase abstracta denominada Cánido modela esta familia de
+* animales. Es una subclase de Animal.
+*/
+public abstract class Cánido extends Animal {
+}
